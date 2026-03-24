0dce362f6c74a18513b4e77c0a372432051fd7d2
ivaslanov <ivaslanov@gmail.com>
2026-03-24 19:39:28 -0300

    Update Welcome.js

--- frontend/src/components/chat/Welcome.js
+++ frontend/src/components/chat/Welcome.js
@@ -7,7 +7,7 @@ export default function Welcome() {
         <WelcomeSVG />
         <div className="text-center">
           <h2 className="text-xl text-gray-500 dark:text-gray-400">
-            Click the 'Match' or 'Start' button to the top left to begin. 
+            Haz clic en el botón “Match” o “Start” en la esquina superior izquierda para comenzar. 
           </h2>
         </div>
       </div>
